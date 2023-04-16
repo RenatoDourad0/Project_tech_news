20e0723ea7ac6447e2600c01e999e0a4f5473463
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-04-16 10:45:19 -0300

    feat: reqs 7, 8, 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,36 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
+def datetime_valid(dt_str):
+    try:
+        datetime.fromisoformat(dt_str)
+    except Exception:
+        raise ValueError("Data inválida")
+    return True
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": f"{title}", "$options": "i"}})
+    if news:
+        return [(elm["title"], elm["url"]) for elm in news]
+    return []
 
 
 # Requisito 8
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date: str):
+    if datetime_valid(date):
+        formatedDate = f"{date[-2:]}/{date[5:7]}/{date[0:4]}"
+        news = search_news({"timestamp": formatedDate})
+    if news:
+        return [(elm["title"], elm["url"]) for elm in news]
+    return []
 
 
 # Requisito 9
-def search_by_category(category):
-    """Seu código deve vir aqui"""
+def search_by_category(category: str):
+    news = search_news({"category": category.lower().capitalize()})
+    if news:
+        return [(elm["title"], elm["url"]) for elm in news]
+    return []
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,32 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+import pytest
+from tech_news.analyzer.reading_plan import (
+    ReadingPlanService,
+)  # noqa: F401, E261, E501
+
+find_news_return = []
 
 
-def test_reading_plan_group_news():
-    pass
+def test_reading_plan_group_news(mocker):
+    mocker.patch("tech_news.database.find_news", return_value=find_news_return)
+
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(-1)
+
+    assert (
+        ReadingPlanService.group_news_for_available_time(15)["readable"][0][
+            "unfilled_time"
+        ]
+        == 1
+    )
+
+    assert ReadingPlanService.group_news_for_available_time(15)["readable"][0][
+        "chosen_news"
+    ] == [
+        (find_news_return[0]["title"], find_news_return[0]["reading_time"]),
+        (find_news_return[1]["title"], find_news_return[1]["reading_time"]),
+    ]
+
+    assert (
+        len(ReadingPlanService.group_news_for_available_time(15)["unreadable"])
+        == 0
+    )
