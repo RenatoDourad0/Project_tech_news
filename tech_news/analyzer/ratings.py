6cf899ad43bd2ab39b4dfdcd5af9a089b98bc28c
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-05-09 08:23:13 -0300

    feta: reqs finais

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,23 @@
+from tech_news.database import find_news
+
+
+def value_getter(item):
+    return item[1]
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    category_list = [post["category"] for post in news]
+
+    category_dict = {}
+    for category in category_list:
+        if category in category_dict:
+            category_dict[f"{category}"] += 1
+        else:
+            category_dict[f"{category}"] = 0
+    # return category_dict.items()
+    sorted_by_value = sorted(
+        category_dict.items(), key=value_getter, reverse=True
+    )[:5]
+    return sorted(sorted_by_value)
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -3,21 +3,38 @@ from tech_news.analyzer.reading_plan import (
     ReadingPlanService,
 )  # noqa: F401, E261, E501
 
-find_news_return = []
+find_news_return = [
+    {
+        "title": "Cabos de rede: o que são, quais os tipos e como crimpar?",
+        "reading_time": 9,
+    },
+    {
+        "title": "Estruturas de repetição: quais as 4 principais e quando usá-las?",
+        "reading_time": 5,
+    },
+    {
+        "title": "Website development: o que é, o que faz e salário! O guia inicial!",
+        "reading_time": 13,
+    },
+]
 
 
 def test_reading_plan_group_news(mocker):
-    mocker.patch("tech_news.database.find_news", return_value=find_news_return)
-
     with pytest.raises(ValueError):
-        ReadingPlanService.group_news_for_available_time(-1)
+        with mocker.patch(
+            "tech_news.database.find_news", return_value=find_news_return
+        ):
+            ReadingPlanService.group_news_for_available_time(-1)
 
-    assert (
-        ReadingPlanService.group_news_for_available_time(15)["readable"][0][
-            "unfilled_time"
-        ]
-        == 1
-    )
+    with mocker.patch(
+        "tech_news.database.find_news", return_value=find_news_return
+    ):
+        assert (
+            ReadingPlanService.group_news_for_available_time(15)["readable"][
+                0
+            ]["unfilled_time"]
+            == 1
+        )
 
     assert ReadingPlanService.group_news_for_available_time(15)["readable"][0][
         "chosen_news"
